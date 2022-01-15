d6558d07cede8adcd264fa8ff168d3ffc8306912
dostonru <rustamovdoston602@gmail.com>
2022-01-16 02:28:02 +0500

    Initial Commit

---
+++ CipherCode/CipherCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CipherCodeApp.swift
+//  CipherCode
+//
+//  Created by Doston Rustamov on 16/01/22.
+//
+
+import SwiftUI
+
+@main
+struct CipherCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
